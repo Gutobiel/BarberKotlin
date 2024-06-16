e9bfde40d9e6b6374d6c0bd809f3d6c6edea17b5
Pauli-b <paulibentes@outlook.com>
2024-06-16 20:53:54 -0300

    Ajustes finais

--- app/src/main/java/com/example/barbearia/ui/theme/Color.kt
+++ app/src/main/java/com/example/barbearia/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val RedColor = Color(0xFFE63946)
+val WhiteColor = Color(0xFFF1FAEE)
+val DarkBlueColor = Color(0xFF1D3557)
+
